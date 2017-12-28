da13e5701717bb76de5b34a255b9685bd671f2a1
Ioakeim <ioaktheo@icloud.com>
2017-12-28 16:00:15 +0200

    Modified URL's of the Home Page and added sessions to the reservation.php

--- php/Reservation.php
+++ php/Reservation.php
@@ -2,12 +2,12 @@
 	mysql_connect('localhost', 'Ioakeim', 'Sunrise2017') or die ("Unable to connect to the Database.");
 	mysql_select_db('Sunrise') or die ("Unable to select the Database");
 
-	$name=$_POST['name'];
-	$surname=$_POST['surname'];
-	$people=$_POST['people'];
-	$ReservationDate=$_POST['ReservationDate'];
-	$ArrivalDate=$_POST['ArrivalDate'];
-	$DepartureDate=$_POST['DepartureDate'];
+	$name=$_SESSION['firstname'];
+	$surname=$_SESSION['surname'];
+	$people=$_SESSION['num_people'];
+	$ArrivalDate=$_SESSION['ar_date'];
+	$DepartureDate=$_SESSION['dep_date'];
+	$ReservationDate=date_default_timezone_get();
 
 	$CustomerQuery="insert into Customer(Name, Surname) values (\"$name\", \"surname\")";
 	mysql_query($CustomerQuery) or die ("Could not Insert Customer");
@@ -17,6 +17,7 @@
 	}
 
 	$ReservationQuery="insert into Reservation(People, ReservationDate, ArrivalDate, DepartureDate, CustomerID) values (\"$people\", \"$ReservationDate\", \"$ArrivalDate\", \"$DepartureDate\", \"$CustomerID\")";
+	
 	mysql_query($ReservationQuery) or die ("Could not complete the Reservation");
 
 	mysql_close();
