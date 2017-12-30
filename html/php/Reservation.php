2690a7c50b5da3cb540f13ee2e88836dc6407159
Ioakeim <ioaktheo@icloud.com>
2017-12-30 21:16:53 +0200

    Another attempt to fix connections issues regarding the database and the reservation script.

--- html/php/Reservation.php
+++ html/php/Reservation.php
@@ -1,10 +1,9 @@
 <?php
 	session_start();
-
-	$conn = new mysqli("localhost", "Ioakeim", "Sunrise2017", "Sunrise");
-
-	if ($conn->connect_error){
-		die("ERROR: Unable to connect: ". $conn->connect_error);
+	$link = mysqli_connect("localhost", "Ioakeim", "Sunrise2017", "Sunrise") 
+	
+	if($link == false){
+		die("ERROR: Could not connect.".mysqli_connect_error());
 	}
 	
 	$name=$_SESSION['firstname'];
@@ -15,15 +14,15 @@
 	$ReservationDate=date_default_timezone_get();
 
 	$CustomerQuery="insert into Customer(Name, Surname) values (\"$name\", \"$surname\")";
-	$result = $conn->query($CustomerQuery) or die ("Could not Insert Customer");
+	$result = mysqli_query($link, $CustomerQuery) or die ("Could not Insert Customer");
 
-	if($result){
-		$CustomerID=conn->query('SELECT CustomerID FROM Customer ORDER BY CustomerID DESC LIMIT 1');
+	if(result){
+		$CustomerID=mysql_query('SELECT CustomerID FROM Customer ORDER BY CustomerID DESC LIMIT 1');
 	}
 
 	$ReservationQuery="insert into Reservation(People, ReservationDate, ArrivalDate, DepartureDate, CustomerID) values (\"$people\", \"$ReservationDate\", \"$ArrivalDate\", \"$DepartureDate\", \"$CustomerID\")";
 	
-	$result = conn->query($ReservationQuery) or die ("Could not complete the Reservation");
+	mysqli_query($link, $ReservationQuery) or die ("Could not complete the Reservation");
 
-	mysql_close();
+	mysqli_close($link);
 ?>
