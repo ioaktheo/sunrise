11e235da53227b3bcddaf147cee5407a5e154ef1
GiwrgosT <tsaridisgiwrgos@yahoo.gr>
2018-01-02 17:22:44 +0200

    Added error variable.

--- html/php/Validation.php
+++ html/php/Validation.php
@@ -9,48 +9,74 @@ if (isset($_POST["firstname"]) && isset($_POST["surname"]) && isset($_POST["ar_d
 	$num_people=htmlspecialchars($_POST["people"]);
 }
 
+$errors=FALSE;
+
 //Firstname validation check
 
 if (empty($firstname)) {
 	echo "Firstname is required!<br>";
+	$errors=TRUE;
 }
 if ((strlen($firstname)>50) || (!ctype_alpha($firstname))) {
 	echo "Firstname is invalid!<br>";
+	$errors=TRUE;
 }
+
 
 //Surname validation check
 
 if (empty($surname)) {
 	echo "Surname is required!<br>";
+	$errors=TRUE;
 }
+
 if ((strlen($surname)>50) || (!ctype_alpha($surname))) {
 	echo "Surname is invalid!<br>";
+	$errors=TRUE;
 }
+
 
 //Arrival date validation check
 if (empty($ar_date)) {
 	echo "Please select your arrival date!<br>";
+	$errors=TRUE;
 }
+
 if ($ar_date>=$dep_date) {
 	echo "Arrival date must be before departure date!<br>";	
+	$errors=TRUE;
 }
+
 if (($ar_date<"2018-04-1") || ($ar_date>"2018-10-30")) {
 	echo "Unavailable date!";
+	$errors=TRUE;
 }
+
 
 //Departure date validation check
 
 if (empty($dep_date)) {
 	echo "Please select your departure date!<br>";
-}
-if ($dep_date<=$ar_date) {
-	echo "Departure date must be after arrival date!<br>";	
-}
-if (($ar_date<"2018-04-1") || ($ar_date>"2018-10-30")) {
-	echo "Unavailable date!";
+	$errors=TRUE;
 }
 
+if ($dep_date<=$ar_date) {
+	echo "Departure date must be after arrival date!<br>";	
+	$errors=TRUE;
+}
+
+if (($ar_date<"2018-04-1") || ($ar_date>"2018-10-30")) {
+	echo "Unavailable date!";
+	$errors=TRUE;
+}
+
+
 //Sending data to Reservation.php
+if (errors) {
+	die("Errors found!!!");
+}
+else{
 $output = shell_exec('Reservation.php');
 echo "<pre>$output</pre>";
+}
 ?>
