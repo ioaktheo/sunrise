b6dc52d5e508006a2b85a12ce424d958d3383d1a
Ioakeim <ioaktheo@icloud.com>
2018-01-02 11:37:26 +0200

    Design & Security improvements

--- html/php/Reservation.php
+++ html/php/Reservation.php
@@ -14,21 +14,20 @@
 
 	$customer_query="insert into Customer(Name, Surname) values (\"$name\", \"$surname\")";
 
+	$reservation_query="insert into Reservation(People, ArrivalDate, DepartureDate, CustomerID) values (\"$people\", \"$arrival_date\", \"$departure_date\", \"$customer_id\")";
+
 	if (mysqli_query($connection, $customer_query)) {
     	$customer_id = mysqli_insert_id($connection);
-    	echo "You can return again by using this customer ID: " . $customer_id;
+
+    	if (mysqli_query($connection, $reservation_query)){
+			$reservation_id = mysqli_insert_id($connection);
+			echo "<h1 align="center">Thank you for your reservation. Your Reservation ID is: " . $reservation_id."</h1><br><br>";
+			echo "<h2 align="center">You can return again by using this customer ID: " .$customer_id."</h2>";
+		}else{
+	 		echo "Error: " . $reservation_query . "<br>" . mysqli_error($connection);
+		}
 	} else {
     	echo "Error: " . $customer_query . "<br>" . mysqli_error($connection);
-	}
-
-	$reservation_query="insert into Reservation(People, ArrivalDate, DepartureDate, CustomerID) values (\"$people\", \"$arrival_date\", \"$departure_date\", \"$customer_id\")";
-	
-	if (mysqli_query($connection, $reservation_query)){
-		$reservation_id = mysqli_insert_id($connection);
-		echo "Thank you for your reservation. Your Reservation ID is: " . $reservation_id;
-
-	}else{
-	 	echo "Error: " . $reservation_query . "<br>" . mysqli_error($connection);
 	}
 
 	mysqli_close($connection);
