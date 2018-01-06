3cf82374983c4a8a345120829dbe1c20a2c93f1a
GiwrgosT <tsaridisgiwrgos@yahoo.gr>
2018-01-06 12:46:22 +0200

    Fixed some bugs for the reservation search

--- html/php/ReservationCheck.php
+++ html/php/ReservationCheck.php
@@ -5,7 +5,8 @@ $connection = mysqli_connect("localhost", "George", "Sunrise2017", "Sunrise");
 		die("Connection failed: ".mysqli_connect_error());
 	}
 	$res_id=htmlspecialchars($_POST["reservation_id"]);
-	$data_query="SELECT Name,Surname,People,ArrivalDate,DeprtureDate FROM Customer,Reservation WHERE ReservationID==$res_id";
+	$data_query="SELECT Name,Surname,People,ArrivalDate,DepartureDate FROM Customer,Reservation WHERE CustomerID=ReservationID.CustomerID AND ReservationID=".$res_id;
+	
 	
 	if (mysqli_query($connection,$data_query)) {
 		$name=$row["Name"];
@@ -20,14 +21,14 @@ $connection = mysqli_connect("localhost", "George", "Sunrise2017", "Sunrise");
 	echo "body{";
 	echo "background-color:#FFFACD";
 	echo "}";
-	echo "/style";
-	echo "/head";
+	echo "</style>";
+	echo "</head>";
 	echo "<body>";
-	echo "<h2>Your Reservation's Informnation:</h2>";
+	echo "<h2>Your Reservation Informnation:</h2>";
 	echo "<br><br>";
 	echo "<table><tr><th>Reservation ID</th><th>Firstname</th><th>Surname</th><th>Number of people</th><th>Arrival Date</th><th>Departure Date</th></tr>";
 	echo "<tr><td>".$res_id"</td><td>".$name"</td><td>".$surname"</td><td>".$people"</td><td>".$arr_date"</td><td>".$dep_date"</td></tr></table>";
-	echo "</body";
+	echo "</body>";
 	echo "</html>";
 	}
 ?>
