868cf80f6740422c5bde214c478d09b3c0615be9
GiwrgosT <tsaridisgiwrgos@yahoo.gr>
2018-01-07 20:32:23 +0200

    Made some visual changes.

--- html/php/Validation.php
+++ html/php/Validation.php
@@ -1,4 +1,10 @@
 <?php
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<body style=\"background-color:#FFFACD\">";
+echo "<div class=\"head\">";
+echo "<img class=\"img-responsive\" src=\"/src/images/header.JPG\">";
+echo "</div><br>";
 if (isset($_POST["firstname"]) && isset($_POST["surname"]) && isset($_POST["ar_date"]) && isset($_POST["dep_date"]) && isset($_POST["people"])) 
 {
 	$firstname=htmlspecialchars($_POST["firstname"]);
@@ -13,11 +19,11 @@ $errors=False;
 //Firstname validation check
 
 if (empty($firstname)) {
-	echo "Firstname is required!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Firstname is required!</h2><br>";
 	$errors=True;
 }
 if ((strlen($firstname)>50) || (!ctype_alpha($firstname))) {
-	echo "Firstname is invalid!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Firstname is invalid!</h2><br>";
 	$errors=True;
 }
 
@@ -25,29 +31,29 @@ if ((strlen($firstname)>50) || (!ctype_alpha($firstname))) {
 //Surname validation check
 
 if (empty($surname)) {
-	echo "Surname is required!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Surname is required!</h2><br>";
 	$errors=True;
 }
 
 if ((strlen($surname)>50) || (!ctype_alpha($surname))) {
-	echo "Surname is invalid!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Surname is invalid!</h2><br>";
 	$errors=True;
 }
 
 
 //Arrival date validation check
 if (empty($ar_date)) {
-	echo "Please select your arrival date!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Please select your arrival date!</h2><br>";
 	$errors=True;
 }
 
 if ($ar_date>=$dep_date) {
-	echo "Arrival date must be before departure date!<br>";	
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Arrival date must be before departure date!</h2><br>";	
 	$errors=True;
 }
 
 //if (($ar_date<"04/01/2018") || ($ar_date>"10/31/2018")) {
-//	echo "Unavailable arrival date!<br>";
+//	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Unavailable arrival date!</h2><br>";
 //	$errors=True;
 //}
 
@@ -55,27 +61,29 @@ if ($ar_date>=$dep_date) {
 //Departure date validation check
 
 if (empty($dep_date)) {
-	echo "Please select your departure date!<br>";
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Please select your departure date!</h2><br>";
 	$errors=True;
 }
 
 if ($dep_date<=$ar_date) {
-	echo "Departure date must be after arrival date!<br>";	
+	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Departure date must be after arrival date!</h2><br>";	
 	$errors=True;
 }
 
 //if (($dep_date<"04/01/2018") || ($dep_date>"10/31/2018")) {
-//	echo "Unavailable departure date!<br>";
+//	echo "<h2 align=\"center\" style=\"color:#A0522D;\">Unavailable departure date!<br></h2>";
 //	$errors=True;
 //}
 
 
 //Sending data to Reservation.php
 if ($errors==True) {
-	exit("Errors found!!!");
+	exit("<h2 align=\"center\" style=\"color:#A0522D;\">Errors found!!!<br>Please check your reservation form!</h2>");
 }
 else{
 include_once("Reservation.php");
 $output = shell_exec('Reservation.php');
 }
+echo "</body>";
+echo "</html>";
 ?>
